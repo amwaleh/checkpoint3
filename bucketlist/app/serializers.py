42d7ce56a9e73cea3df2bde93787456cc3dbcd98
Mwaleh muturi <alex.mwaleh@andela.com>
2016-01-04 12:14:04 +0300

    (Chore) Add Serializers Added User serilizers Added userviews Added Token settings Added url for getting token

---
+++ bucketlist/app/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from models import Bucketitems, Bucketlist
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+	'''
+		Serializer for user authenictaion
+	'''
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+class BuckeitemSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Bucketitems
+		fileds = ('blist', 'name', 'done', 'created_on', 'modified_on')
+
+class BucketlistSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Bucketlist
+		fields = ('id','name','created_on', 'modified_on')
--- bucketlist/bucketlist/urls.py
+++ bucketlist/bucketlist/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from rest_framework.authtoken import views
+from app import views
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+url(r'^admin/', include(admin.site.urls)),
+url(r'^users/', views.UserList.as_view()),
+url(r'^api-token', 'rest_framework.authtoken.views.obtain_auth_token'),
+)
+
