093af687885f15dfe16ef630df2f78031d58d670
Mwaleh muturi <alex.mwaleh@andela.com>
2016-01-08 18:25:03 +0300

    (Feature) Add Static Files - Added Static Files - Added Materialise CSS

--- bucketlist/web/urls.py
+++ bucketlist/web/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from views import index
+from views import index, listBucketlists, login
 
 urlpatterns = patterns('',
 
-    url(r'^', index),
+    url(r'^/', index),
+    url(r'^bucketlists/$',listBucketlists),
+    url(r'^login/$',login),
 )
--- bucketlist/web/views.py
+++ bucketlist/web/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 import requests
+import json
+
+domain = 'http://127.0.0.1:8000'
 
 def index(request):
 	form = "welcome"
 	return render(request, 'index.html', {'form': form})
+def login(request):
+	if request.method == "GET":
+		return render(request, 'signin.html',)
+
+	if request.method == "POST":
+		bucketlists = requests.post(domain + '/api/users/')
+		form = bucketlists
+		return render(request, 'index.html', {'form': form})
+	
+
+
+
+
+def listBucketlists(request):
+	if request.method == 'GET':
+		#import ipdb; ipdb.set_trace()
+		bucketlists = requests.get(domain + '/api/bucketlists/')
+		lists =bucketlists.json()
+		return render(request, 'bucketlist.html', {'lists':lists })
